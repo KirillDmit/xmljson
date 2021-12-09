c054e8f521245d2db3717f58076f32a612e04df9
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-09 19:02:39 +0500

    Update api2.py

--- api2.py
+++ api2.py
@@ -8,8 +8,8 @@ def get_date(x):
     return datetime.datetime.strptime(x['timestamp'], '%Y-%m-%dT%H:%M:%SZ').date()
 
 
-url = 'https://ru.m.wikipedia.org/wiki/%D0%91%D0%B5%D0%BB%D1%8C%D0%BC%D0%BE%D0%BD%D0%B4%D0%BE,' \
-      '_%D0%96%D0%B0%D0%BD-%D0%9F%D0%BE%D0%BB%D1%8C '
+url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%91%D0%B5%D0' \
+      '%BB%D1%8C%D0%BC%D0%BE%D0%BD%D0%B4%D0%BE_%D0%96%D0%B0%D0%BD-%D0%9F%D0%BE%D0%BB%D1%8C '
 stat = {}
 data = loads(urlopen(url).read().decode('utf8'))
 group = groupby(data['query']['pages']['192203']['revisions'], get_date)
