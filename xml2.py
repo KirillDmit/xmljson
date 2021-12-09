a4ac4a27fd1313c2392473282de723eb0db71e56
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-09 17:03:46 +0500

    upd xml tasks

--- xml2.py
+++ xml2.py
@@ -3,21 +3,16 @@ from urllib.request import urlopen
 import json
 
 
-def main():
-    data = urlopen('https://lenta.ru/rss').read().decode('utf8')
-    root = ET.fromstring(data)
-    items = root.find('channel').findall('item')
-    select(items)
-
-
-def select(items):
-    result = []
-    for item in items:
-        data = {}
-        for i in item.iter():
-            if i != item:
-                data[i.tag] = i.text
-        result.append(data)
-    with open('news2.json', 'w', encoding='utf8') as f:
-        json.dump(result, f)
+data = urlopen('https://lenta.ru/rss').read().decode('utf8')
+root = ET.fromstring(data)
+items = root.find('channel').findall('item')
+result = []
+for item in items:
+    data = {}
+    for i in item.iter():
+        if i != item:
+            data[i.tag] = i.text
+    result.append(data)
+with open('news2.json', 'w', encoding='utf8') as f:
+    json.dump(result, f)
 
