71802a3813ec43c7be520cbb2ef0abdeafd909c5
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-09 19:00:46 +0500

    Create api2.py

---
+++ api2.py
@@ -0,0 +1,23 @@
+import datetime
+from itertools import groupby
+from urllib.request import urlopen
+from json import loads
+
+
+def get_date(x):
+    return datetime.datetime.strptime(x['timestamp'], '%Y-%m-%dT%H:%M:%SZ').date()
+
+
+url = 'https://ru.m.wikipedia.org/wiki/%D0%91%D0%B5%D0%BB%D1%8C%D0%BC%D0%BE%D0%BD%D0%B4%D0%BE,' \
+      '_%D0%96%D0%B0%D0%BD-%D0%9F%D0%BE%D0%BB%D1%8C '
+stat = {}
+data = loads(urlopen(url).read().decode('utf8'))
+group = groupby(data['query']['pages']['192203']['revisions'], get_date)
+for date, changes in group:
+    stat.update({date: len(list(changes))})
+
+for date in stat:
+    print(date, stat[date])
+
+
+
