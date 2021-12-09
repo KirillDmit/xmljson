cfa68b569c5fe3e21d46a3b93b4a3e52fceec5a3
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-09 18:20:45 +0500

    add api task

--- api.py
+++ api.py
@@ -1,7 +1,23 @@
+import datetime
+from itertools import groupby
 from urllib.request import urlopen
 from json import loads
 
 
-url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%93%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B8%D0%B9,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%91%D0%BE%D1%80%D0%B8%D1%81%D0%BE%D0%B2%D0%B8%D1%87'
+def get_date(x):
+    return datetime.datetime.strptime(x['timestamp'], '%Y-%m-%dT%H:%M:%SZ').date()
+
+
+url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%93%D1%80%D0' \
+      '%B0%D0%B4%D1%81%D0%BA%D0%B8%D0%B9,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%91%D0%BE%D1%80' \
+      '%D0%B8%D1%81%D0%BE%D0%B2%D0%B8%D1%87 '
+stat = {}
 data = loads(urlopen(url).read().decode('utf8'))
-print(data['query']['pages']['183903']['revisions'][0])
+group = groupby(data['query']['pages']['183903']['revisions'], get_date)
+for date, changes in group:
+    stat.update({date: len(list(changes))})
+with open('api.txt', 'w', encoding='utf8') as file:
+    for date in stat:
+        print(date, stat[date], file=file)
+
+
